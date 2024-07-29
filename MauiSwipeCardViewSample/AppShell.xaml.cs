882e77b7efafd6be6cb1cb0df57217945769ae64
Gerald Versluis <gerald.versluis@microsoft.com>
2024-07-29 10:28:23 +0200

    Initial commit

---
+++ MauiSwipeCardViewSample/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiSwipeCardViewSample
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# MauiSwipeCardViewSample
+ Sample code to demonstrate how to implement Plugin.Maui.SwipeCardView in .NET MAUI
